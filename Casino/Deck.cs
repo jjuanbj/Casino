824fb0b21d11d755dcbc2584c77cab799f1e24f3
Juan Jiménez <jjuanbj@hotmail.com>
2020-09-23 10:33:19 -0400

    Update Deck

--- Casino/Deck.cs
+++ Casino/Deck.cs
@@ -1,4 +1,3 @@
-using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,148 +5,63 @@ using System.Text;
 
 namespace Casino
 {
-    class Player
+    class Deck
     {
-        public string Name { get; set; }
-        public List<Card> Cards { get; set; }
-        public ConsoleOutput ConsoleOutput { get; set; }
-        public List<Card> CapturedCards { get; set; }
+        private List<Card> DeckCards { get; set; }
 
-        public Player(string name, ConsoleOutput consoleOutput) {
-            Name = name;
-            ConsoleOutput = consoleOutput;
-            CapturedCards = new List<Card>();
-        }
+        public void CreateCards()
+        {            
+            var rankValue = Enum.GetValues(typeof(Rank));
+            var suitValue = Enum.GetValues(typeof(Suit));
 
-        public void Play(Table table)
-        {
-            Card card = SelectYourCard();
+            DeckCards = new List<Card>();
 
-            ConsoleOutput.ChooseOneAction();
-            
-            string userinput = "";
-
-            while (userinput != Keyboard.ONE && userinput != Keyboard.TWO){
-                
-                userinput = Console.ReadLine().Trim();
-
-                if (userinput != Keyboard.ONE && userinput != Keyboard.TWO){
-                    ConsoleOutput.ThisIsNotAnAllowedAction();
-                    ConsoleOutput.ChooseOneAction();
-                }
-            }            
-
-            switch (userinput)
+            foreach (Suit suit in suitValue)
             {
-                case Keyboard.ONE:
-                    ThrowTheCardToTheTable(card, table);
-                    break;
-                case Keyboard.TWO:
-                    TakeCardFromTheTable(card, table);
-                    break;
+                foreach (Rank rank in rankValue)
+                {
+                    DeckCards.Add(new Card(rank, suit));
+                }
             }            
         }
 
-        private Card SelectYourCard()
-        {                   
-            ConsoleOutput.SelectOneCardByIndexNumber(this);            
-
-            string cardNumber = "";
-            Card card = null;
-
-            while (String.IsNullOrEmpty(cardNumber))
-            {
-                cardNumber = Console.ReadLine().Trim();
-
-                if (!String.IsNullOrEmpty(cardNumber)
-                && cardNumber.All(char.IsDigit)
-                && Enumerable.Range((int)General.Zero, Cards.Count).Contains(Int32.Parse(cardNumber)))
-                {
-                    ConsoleOutput.YouSelected(this.Cards, cardNumber);
-
-                    card = new Card(Cards.ElementAt(Int32.Parse(cardNumber)).CardName);
-                }
-                else
-                {
-                    ConsoleOutput.TypeValidCardNumber();
-                    cardNumber = "";
-                    continue;                    
-                }
-            }
-            return card;
-        }
-
-        private void ThrowTheCardToTheTable(Card card, Table table)
+        public void ShuffleCards()
         {
-            table.Cards.Add(card);
-            Cards.RemoveAll(c => c.CardName == card.CardName);
-
-            ConsoleOutput.ShowTableCards(table);
-
-            ConsoleOutput.ShowPlayerCards(this);            
-        }
-
-        private void TakeCardFromTheTable(Card selectedCard, Table table)
-        {
-            ConsoleOutput.WhichCardWouldYouLikeToTakeFromTheTable(table);
-            ConsoleOutput.PressFWhenFinished();
+            var shuffledCards = new List<Card>();
             
-            string cardRank = "";
-            
-            List<Card> tableCards = new List<Card>();
-
-            while (cardRank != Keyboard.UPPERCASE_F && cardRank != Keyboard.LOWERCASE_F)
+            for (int i = 0; i < (int)General.TotalCards; i++)
             {
-                cardRank = Console.ReadLine().Trim();
-                
-                if(String.IsNullOrEmpty(cardRank)){
-                    ConsoleOutput.TypeValidCardNumber();                    
-                    continue;
-                } else if (cardRank != Keyboard.UPPERCASE_F 
-                        && cardRank != Keyboard.LOWERCASE_F
-                        && cardRank.All(char.IsDigit)
-                        && Enumerable.Range((int)General.Zero, table.Cards.Count).Contains(Int32.Parse(cardRank)))
-                        {
-                    ConsoleOutput.YouSelected(table.Cards, cardRank);
-                                
-                    tableCards.Add(table.Cards.ElementAt(Int32.Parse(cardRank)));                    
-                }  else if (cardRank == Keyboard.UPPERCASE_F || cardRank == Keyboard.LOWERCASE_F){
-                    break;                
-                }
-                else
-                {
-                    ConsoleOutput.TypeValidCardNumber();
-                    cardRank = "";
-                    continue;
-                }                
-            }   
-
-            if(selectedCard.Rank == Rank.Ace){
-                if (tableCards.Sum(c => Convert.ToInt32(c.Rank)) % 14 == 0 
-                 || tableCards.Sum(c => Convert.ToInt32(c.Rank) == 1 ? 14 : Convert.ToInt32(c.Rank)) % 14 == 0)
-                {
-                    table.Cards.RemoveAll(c => tableCards.Contains(c));
-                    CapturedCards.AddRange(tableCards);
-                    CapturedCards.Add(selectedCard);
-
-                    ConsoleOutput.ShowTableCards(table);
-                    ConsoleOutput.ShowCapturedCards(this);    
-                }
+                Random random = new Random();
+                int index = random.Next(DeckCards.Count);
+                shuffledCards.Add(DeckCards[index]);
+                DeckCards.RemoveAt(index);
             }
 
-            /*if (!tableCards.Any() || tableCards.Sum(c => Convert.ToInt32(c.Rank)) % Convert.ToInt32(selectedCard.Rank) != 0)
-            {
-                ConsoleOutput.YouJustLostYourCardBecauseItIsInvalid();
-                ThrowTheCardToTheTable(selectedCard, table);
-            } else if (tableCards.Sum(c => Convert.ToInt32(c.Rank)) % Convert.ToInt32(selectedCard.Rank) == 0)
-            {
-                table.Cards.RemoveAll(c => tableCards.Contains(c));
-                CapturedCards.AddRange(tableCards);
-                CapturedCards.Add(selectedCard);
+            DeckCards = shuffledCards;           
+        }
 
-                ConsoleOutput.ShowTableCards(table);
-                ConsoleOutput.ShowCapturedCards(this);
-            }*/
+        public void DealCardsPlayer(List<Player> Players)
+        {
+            foreach (var player in Players)
+            {                
+                player.Cards = DeckCards.Take((int)General.NumberCardsToDeal).ToList();
+                DeckCards.RemoveRange((int)General.Zero, (int)General.NumberCardsToDeal);
+
+                // Dealing with Ace cards rank
+                if (player.Name == "Juan")
+                {
+                    player.Cards.Add(new Card(Rank.Ace, Suit.Diamond));
+                }
+            }            
+        }
+        
+        public void DealCardsTable(Table table)
+        {
+            table.Cards = DeckCards.Take((int)General.NumberCardsToDeal).ToList();
+            DeckCards.RemoveRange((int)General.Zero, (int)General.NumberCardsToDeal);
+
+            // Dealing with Ace cards value
+            table.Cards.Add(new Card(Rank.Ace, Suit.Club));
         }
     }
 }
