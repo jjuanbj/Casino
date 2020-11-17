170db47e3391f0423924adcbd9e00b01835a293f
Juan Jiménez <jjuanbj@hotmail.com>
2020-11-17 17:45:37 -0400

    Count captured cards

--- Casino/Counter.cs
+++ Casino/Counter.cs
@@ -1,6 +1,20 @@
-// This class will counts all points of the game
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Casino
 {
     class Counter
-    { }
+    {
+
+        public void CountPointsForIndividualPlayers(List<Player> players){
+
+            // TODO: check which player has more cards
+            if (players.Count > 2 || (players.Count == 2 && players.SelectMany(p => p.CapturedCards)
+                                                                   .Count() != (int)General.TotalCards / 2))
+            {
+                Player player = players.Where(p => (int)General.TotalCards / p.CapturedCards.Count > 
+                                                   (int)General.TotalCards / p.CapturedCards.Count).FirstOrDefault();    
+            }
+        }
+    }
 }
