6a99c16464d190c876209e6361a773273eef5a41
Juan Jiménez <jjuanbj@hotmail.com>
2020-12-01 17:48:16 -0400

    ACE_MAX_VALUE

--- Casino/Utils/Constants.cs
+++ Casino/Utils/Constants.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Casino
@@ -8,5 +8,7 @@ namespace Casino
         public const string Computer = "Computer";
         public const string WelcomeToCasinoGame = "Welcome to casino game!";
         public const string ChooseYourLanguageEnglishEspañol = "Choose your language: 1- English, 2- Español";
+
+        public const int ACE_MAX_VALUE = 14;
     }
 }
