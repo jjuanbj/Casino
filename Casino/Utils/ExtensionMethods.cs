73557556a1a879218586d3519091200e370b9c8f
Juan Jiménez <jjuanbj@hotmail.com>
2020-12-04 14:22:21 -0400

    GetEnumDisplayName

--- Casino/Utils/ExtensionMethods.cs
+++ Casino/Utils/ExtensionMethods.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
 
 namespace Casino.Extension
 {
@@ -21,6 +24,15 @@ namespace Casino.Extension
                     }
                 }
             }
+        }
+
+        public static string GetEnumDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetName();
         }        
     }
 }
